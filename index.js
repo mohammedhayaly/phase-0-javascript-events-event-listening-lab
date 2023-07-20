b56250d740e7d5fdbaeb172a8e8b213afedfeae2
mohammedhayaly <mohammed.alhayali88@yahoo.com>
2023-07-20 09:51:32 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
-}
+    const button = document.getElementById('button');
+  
+    if (button) {
+      button.addEventListener('click', function() {
+        alert('I was clicked!');
+      });
+    } else {
+      console.error("Element with ID 'button' not found.");
+    }
+  }
